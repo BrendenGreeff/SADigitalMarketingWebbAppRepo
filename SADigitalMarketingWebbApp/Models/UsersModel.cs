3d5dc33f3d3ea9c45bee788f440531c293fc45c8
BrendenGreeff <brendengreeffie@gmail.com>
2021-11-10 19:42:37 +0200

    Commit before checking if my database connection functions

---
+++ SADigitalMarketingWebbApp/Models/UsersModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SADigitalMarketingWebbApp.Models
+{
+    public class UsersModel
+    {
+        public int User_ID { get; set; }
+        public string User_Fname { get; set; }
+        public string User_Lname { get; set; }
+        public string User_Username { get; set; }
+        public string User_Email{ get; set; }
+        public int User_PhoneNumber { get; set; }
+        public DateTime User_DOB { get; set; }
+        public int User_Password { get; set; }
+        public int User_AccessLevel { get; set; }
+    }
+}
