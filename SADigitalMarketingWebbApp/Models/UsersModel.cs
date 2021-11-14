57e875871fbad2c910d65f45e49a7398357a05ad
BrendenGreeff <brendengreeffie@gmail.com>
2021-11-14 22:05:26 +0200

    Signup page is working with validation

--- SADigitalMarketingWebbApp/Models/UsersModel.cs
+++ SADigitalMarketingWebbApp/Models/UsersModel.cs
@@ -11,10 +11,10 @@ namespace SADigitalMarketingWebbApp.Models
         public string Users_Fname { get; set; }
         public string Users_Lname { get; set; }
         public string Users_Username { get; set; }
-        public string Usesr_Email{ get; set; }
-        public int Users_PhoneNumber { get; set; }
+        public string Users_Email{ get; set; }
+        public string Users_PhoneNumber { get; set; }
         public DateTime Users_DOB { get; set; }
-        public int Users_Password { get; set; }
-        public int Users_AccessLevel { get; set; }
+        public string Users_Password { get; set; }
+        
     }
 }
